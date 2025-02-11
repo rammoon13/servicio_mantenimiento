17f118a19ff3718e0f743ed6c4b777527b268b76
rammoon13 <herreraroblesramon@gmail.com>
2025-02-11 09:44:37 +0100

    Editado archivo __manifest__

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# servicio_manteminiento/__manifest__.py
+{
+ 'name': 'Servicio de Mantenimiento',
+ 'version': '1.0',
+ 'author': 'Ramon Herrera',
+ 'category': 'Custom',
+ 'summary': 'Gestión de Mantenimiento de Productos',
+ 'depends': ['base', 'garantia.producto'],
+ 'data': [
+ 'security/ir.model.access.csv',
+ 'views/servicio_mantenimiento_views.xml',
+ ],
+ 'icon': '/garantias/static/description/icon55.png',
+ 'installable': True,
+ 'application': True,
+}
